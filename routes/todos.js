203f4a4934f42c818750b36ddb5f1253865db30a
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-09-19 12:31:47 +0100

    post and get route handler implemented

--- routes/todos.js
+++ routes/todos.js
@@ -11,4 +11,61 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    console.log(req.body)
+    let slug = req.body.name
+    console.log('slug', slug);
+    try {
+        let todo = await db.Todo.create(req.body)
+        res.status(201).json({
+            status: 'success',
+            data: {
+                todo
+            }
+        })
+    } catch (e) {
+        res.status(400).json({
+            status: 'fail',
+            message: e
+        })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        let todo = await db.Todo.findById(req.params.id)
+        res.status(200).json({
+            status: 'success',
+            data: {
+                todo
+            }
+        })
+    } catch (e) {
+        res.status(400).json({
+            status: 'fail',
+            message: e
+        })
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+    let todo = await db.Todo.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+             res.status(203).json({
+                status: 'success',
+                data: {
+                    todo
+                }
+            })
+    } catch (e) {
+        res.status(400).json({
+            status: 'fail',
+            message: e
+        })
+    }
+})
+
+
 module.exports = router;
